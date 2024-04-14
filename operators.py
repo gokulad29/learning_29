eb487c8ec1f77017a268fcf91c1f97f957aa25ff
gokulad29 <158982377+gokulad29@users.noreply.github.com>
2024-04-14 19:23:48 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,17 @@
+#arithmatic
+print(4+3)
+print(4*3)
+print(12/4)
+print(4-3)
+print(4**3)
+#comparison
+print(4==3)
+print(4>=3)
+print(4<=3)
+#logical
+print(4==4 or 4<3)
+print(4==4 and 4<3)
+print(not 4==4)
+
+print(1 in [2,3,4,5,1])
+print("car" in "my car")
